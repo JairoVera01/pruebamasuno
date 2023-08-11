bbe0f71ace8eecf788b2fbd7b11ce18320f18a38
Frank Jairo Vera <fj.verachamorro@gmail.com>
2023-08-11 10:58:59 -0500

    Instalacion de Fuente QuickSand y avance de Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,12 @@
-
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero__wrapper container">
+        <div className="hero__content">
+          <h1>Cyber Days en Más Uno</h1>
+        </div>
+        <div className="hero__form">Item2</div>
+      </div>
+    </section>
+  );
 }
