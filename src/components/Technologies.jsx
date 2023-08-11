879b29f614ec5c34897c57667df026de41b08f30
Frank Jairo Vera <fj.verachamorro@gmail.com>
2023-08-11 14:16:41 -0500

    Configuracion de Slide

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,5 +1,9 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import slide1 from "../assets/images/slide-1.jpg";
+import slide2 from "../assets/images/slide-2.jpg";
+import slide3 from "../assets/images/slide-3.jpg";
 import LogoMasUno from "../assets/images/IconBrand.png";
+import iconslide1 from "../assets/images/icon-slide-1.png";
 export default function Technologies() {
   return (
     <section className="technologies">
@@ -24,9 +28,62 @@ export default function Technologies() {
               interval: 3000,
             }}
           >
-            <SplideSlide>hola 1</SplideSlide>
-            <SplideSlide>hola 2</SplideSlide>
-            <SplideSlide>hola 2</SplideSlide>
+            <SplideSlide>
+              <div className="technologies__slide">
+                <div className="technologies__slide__content">
+                  <img src={iconslide1} alt="" />
+                  <h3>PHP</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur. Cursus cursus erat
+                    massa vel id nunc. Ultricies dui pharetra laoreet laoreet
+                    laoreet habitasse iaculis lobortis. Amet diam dolor
+                    tincidunt congue sagittis in.
+                  </p>
+                  <button className="technologies__button">
+                    Ver tecnología
+                  </button>
+                </div>
+                <img src={slide1} alt="" />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className="technologies__slide">
+                <div className="technologies__slide__content">
+                  <img src={iconslide1} alt="" />
+                  <h3>Flutter</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur. Cursus cursus erat
+                    massa vel id nunc. Ultricies dui pharetra laoreet laoreet
+                    laoreet habitasse iaculis lobortis. Amet diam dolor
+                    tincidunt congue sagittis in.
+                  </p>
+                  <button className="technologies__button">
+                    Ver tecnología
+                  </button>
+                </div>
+                <img src={slide2} alt="" />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className="technologies__slide">
+                <div className="technologies__slide__content">
+                  <img src={iconslide1} alt="" />
+                  <h3>Diseño UX/UI</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur. Cursus cursus erat
+                    massa vel id nunc. Ultricies dui pharetra laoreet laoreet
+                    laoreet habitasse iaculis lobortis. Amet diam dolor
+                    tincidunt congue sagittis in.
+                  </p>
+                  <button className="technologies__button">
+                    Ver tecnología
+                  </button>
+                </div>
+                <img src={slide3} alt="" />
+              </div>
+            </SplideSlide>
           </Splide>
         </div>
       </div>
