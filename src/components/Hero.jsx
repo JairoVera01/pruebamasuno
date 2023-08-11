0d0ca4982a7d59a02672faff3514ab185964d160
Frank Jairo Vera <fj.verachamorro@gmail.com>
2023-08-11 11:11:02 -0500

    Avance Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
+import icon1 from "../assets/images/IconosHero.png";
 export default function Hero() {
   return (
     <section className="hero">
       <div className="hero__wrapper container">
         <div className="hero__content">
-          <h1>Cyber Days en Más Uno</h1>
+          <div className="hero__icons-1">
+            <img src={icon1} alt="" />
+            <span className="hero__icons-1__text">ES HORA DE</span>
+          </div>
+          <h1 className="hero__title">
+            CYBER DAYS <br /> EN Más Uno
+          </h1>
+          <p className="hero__dates">Del 28 al 30 de marzo</p>
+          <div className="hero__cupon">
+            <span>Registrate y obten</span>
+          </div>
         </div>
         <div className="hero__form">Item2</div>
       </div>
